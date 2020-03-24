08c11079cae03e72f5ae8e65ab10d85c51626d89
Li Jung Huang <lijunghuang@Lis-MacBook-Pro.local>
2020-03-24 13:27:42 -0700

    lecture

--- src/App.js
+++ src/App.js
@@ -1,39 +1,53 @@
 import React, { Component } from "react";
 import "./App.css";
 
-class App extendz component {
-  contsructor(props) {
+class App extends Component {
+  constructor(props) {
+    super(props);
     this.state = {
       friendsList: ["john", "jacob", "jingle", "heimer", "schmidt"],
       friend: ""
     };
+    this.changeHandler = this.changeHandler.bind(this);
+    this.addFriend = this.addFriend.bind(this);
   }
+
   changeHandler(event) {
     this.setState({
-      friend: event
+      friend: event.target.value
     });
   }
 
   addFriend() {
-    this.state.friendsList.push(this.state.friend);
+    // this.state.friendsList.push(this.state.friend);
+    this.setState({
+      friendsList: [...this.state.friendsList, this.state.friend],
+      friend: ''
+    })
   }
 
   deleteFriend(index) {
-    this.state.friendsList.splice(index, 1);
+    console.log('delete button fired')
+    // this.state.friendsList.splice(index, 1);
+    const filtered = this.state.friendsList.slice()
+    filtered.splice(index, 1)
+    this.setState({
+      friendsList: filtered
+    })
   }
 
   render() {
-    const mappedFreinds = this.state.friendsList.map((friend, index) => {
-      <div>
+    const mappedFriends = this.state.friendsList.map((friend, index) => {
+      return <div>
         <span key={index}>{friend}</span>
-        <button onClick={this.state.deleteFriend} />
+        <button onClick={() => this.deleteFriend(index)} />
       </div>;
     });
     return (
       <div className="App">
-        <div>{mappedFreinds}</div>
+        <div>{mappedFriends}</div>
         <input
-          type="number"
+          type="text"
           onChange={this.changeHandler}
           value={this.state.friend}
         />
@@ -42,5 +56,5 @@ class App extendz component {
     );
   }
 }
-}
+
 export default App;
